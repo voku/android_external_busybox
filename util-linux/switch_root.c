f104f80144e665e95d285642f046071282d036c2
Denys Vlasenko <vda.linux@googlemail.com>
2012-03-08 07:28:24 +0800

    libbb: make xchroot do a chdir("/") after chroot
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- util-linux/switch_root.c
+++ util-linux/switch_root.c
@@ -105,7 +105,7 @@ int switch_root_main(int argc UNUSED_PARAM, char **argv)
 	}
 	xchroot(".");
 	// The chdir is needed to recalculate "." and ".." links
-	xchdir("/");
+	/*xchdir("/"); - done in xchroot */
 
 	// If a new console specified, redirect stdin/stdout/stderr to it
 	if (console) {
